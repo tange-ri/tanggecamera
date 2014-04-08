4a623f344deb763d2eea603567ad77a7fa78d266
Eri Tange <eri@eri-no-mbp.lan>
2014-04-08 21:35:51 +0900

    Initial Commit

---
+++ TangeCamera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TangeCamera
+//
+//  Created by Eri Tange on 2014/04/08.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TangeCamera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TangeCamera
+//
+//  Created by Eri Tange on 2014/04/08.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
