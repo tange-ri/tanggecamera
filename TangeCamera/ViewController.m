12c26bdb0e93e5e0931bd4a7ba2a41703a1cbb67
Iwark <iwark02@gmail.com>
2014-04-16 23:21:07 +0900

    fixed error

--- TangeCamera/ViewController.m
+++ TangeCamera/ViewController.m
@@ -12,28 +12,27 @@
 #import "DataModels.h"
 #import "SBJson4.h"
 
+#define API_KEY @"025ede102232126ca5139975832cae92"
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-@synthesize locationManager;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
     
     //画面の縦横比を変えずに表示
     [imageView setContentMode:UIViewContentModeScaleAspectFit];
     
-    locationManager = [[CLLocationManager alloc] init];
+    _locationManager = [[CLLocationManager alloc] init];
     
-    // 位置情報サービスが利用できるかどうかをチェック
+    // 位置情報サービスが利用できれば開始
     if ([CLLocationManager locationServicesEnabled]) {
-        locationManager.delegate = self; // ……【1】
-        // 測位開始
-        [locationManager startUpdatingLocation];
+        _locationManager.delegate = self;
+        [_locationManager startUpdatingLocation];
     } else {
         NSLog(@"Location services not available.");
     }
@@ -123,11 +122,6 @@
 - (void)locationManager:(CLLocationManager *)manager
     didUpdateToLocation:(CLLocation *)newLocation
            fromLocation:(CLLocation *)oldLocation {
-    
-    //緯度・経度を出力
-    NSLog(@"didUpdateToLocation latitude=%f, longitude=%f",
-          [newLocation coordinate].latitude,
-          [newLocation coordinate].longitude);
 }
 
 // 測位失敗時や、5位置情報の利用をユーザーが「不許可」とした場合などに呼ばれる
@@ -315,27 +309,28 @@
     
     //お天気を取得
     //OpenWeatherMapのAPI_KEYを作成
-    static NSString *API_KEY=@"YOUR_API_KEY_HERE";
     JFWeatherManager *weatherManager = [[JFWeatherManager alloc]init];
     
+    float latitude = _locationManager.location.coordinate.latitude;
+    float longitude = _locationManager.location.coordinate.longitude;
     
-//    [weatherManager fetchWeatherDataForLatitude:toAdd.coordinate.latitude andLongitude:toAdd.coordinate.longitude withAPIKeyOrNil:API_KEY :^(JFWeatherData *returnedWeatherData){
-//        
-//        NSLog(@"Latitude %.3f",[returnedWeatherData latitudeCoordinateOfRequest]);
-//        NSLog(@"Longitude %.3f",[returnedWeatherData longitudeCoordinateOfRequest]);
-//        NSLog(@"Conditions are %@",[returnedWeatherData currentConditionsTextualDescription]);
-//        NSLog(@"Temperature is %f",[returnedWeatherData temperatureInUnitFormat:kTemperatureCelcius]);
-//        NSLog(@"Sunrise is %@",[returnedWeatherData sunriseTime]);
-//        NSLog(@"Sunset is %@",[returnedWeatherData sunsetTime]);
-//        NSLog(@"Hours of Day Light are %@",[returnedWeatherData dayLightHours]);
-//        NSLog(@"Humidity is %@",[returnedWeatherData humidityPercentage]);
-//        NSLog(@"Pressure is %0.1f",[returnedWeatherData pressureInUnitFormat:kPressureHectopascal]);
-//        NSLog(@"Wind Speed is %0.1f",[returnedWeatherData windSpeedInUnitFormat:kWindSpeedMPH]);
-//        NSLog(@"Wind Direction is %@",[returnedWeatherData windDirectionInGeographicalDirection]);
-//        NSLog(@"Cloud Coverage %@",[returnedWeatherData cloudCovergePercentage]);
-//        NSLog(@"Rainfall Over Next 3h is %0.1fmm",[returnedWeatherData rainFallVolumeOver3HoursInMillimeters]);
-//        NSLog(@"SnowFall Over Next 3h is %0.1fmm",[returnedWeatherData snowFallVolumeOver3HoursInMillimeters]);
-//    }];
+    [weatherManager fetchWeatherDataForLatitude:latitude andLongitude:longitude withAPIKeyOrNil:API_KEY :^(JFWeatherData *returnedWeatherData){
+        
+        NSLog(@"Latitude %.3f",[returnedWeatherData latitudeCoordinateOfRequest]);
+        NSLog(@"Longitude %.3f",[returnedWeatherData longitudeCoordinateOfRequest]);
+        NSLog(@"Conditions are %@",[returnedWeatherData currentConditionsTextualDescription]);
+        NSLog(@"Temperature is %f",[returnedWeatherData temperatureInUnitFormat:kTemperatureCelcius]);
+        NSLog(@"Sunrise is %@",[returnedWeatherData sunriseTime]);
+        NSLog(@"Sunset is %@",[returnedWeatherData sunsetTime]);
+        NSLog(@"Hours of Day Light are %@",[returnedWeatherData dayLightHours]);
+        NSLog(@"Humidity is %@",[returnedWeatherData humidityPercentage]);
+        NSLog(@"Pressure is %0.1f",[returnedWeatherData pressureInUnitFormat:kPressureHectopascal]);
+        NSLog(@"Wind Speed is %0.1f",[returnedWeatherData windSpeedInUnitFormat:kWindSpeedMPH]);
+        NSLog(@"Wind Direction is %@",[returnedWeatherData windDirectionInGeographicalDirection]);
+        NSLog(@"Cloud Coverage %@",[returnedWeatherData cloudCovergePercentage]);
+        NSLog(@"Rainfall Over Next 3h is %0.1fmm",[returnedWeatherData rainFallVolumeOver3HoursInMillimeters]);
+        NSLog(@"SnowFall Over Next 3h is %0.1fmm",[returnedWeatherData snowFallVolumeOver3HoursInMillimeters]);
+    }];
     
     
     //UIImageを生成(image)
